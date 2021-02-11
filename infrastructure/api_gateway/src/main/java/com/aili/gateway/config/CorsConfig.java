d30dbff2fcc20a7a568b26f4b599cafb6bca42c5
ElzatAlim <elzat@foxmail.com>
2021-02-11 17:38:44 +0800

    Version1.2

--- infrastructure/api_gateway/src/main/java/com/aili/gateway/config/CorsConfig.java
+++ infrastructure/api_gateway/src/main/java/com/aili/gateway/config/CorsConfig.java
@@ -23,15 +23,6 @@ import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 import org.springframework.web.util.pattern.PathPatternParser;
 import reactor.core.publisher.Mono;
-
-/**
- * <p>
- * 处理跨域
- * </p>
- *
- * @author qy
- * @since 2019-11-21
- */
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
